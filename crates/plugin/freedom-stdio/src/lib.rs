b1627e765b7c6e76c2506e13782fd760d69b8595
ProfLander <1253239+ProfLander@users.noreply.github.com.>
2025-10-12 02:25:49 +0100

    Add skeletal `freedom-stdio` crate

---
+++ crates/plugin/freedom-stdio/src/lib.rs
@@ -0,0 +1,17 @@
+use freedom::scheme::steel::steel_vm::{builtin::BuiltInModule, register_fn::RegisterFn};
+
+#[unsafe(no_mangle)]
+pub fn module() -> BuiltInModule {
+    let mut module = BuiltInModule::new("freedom/stdio");
+    module.register_fn("#%write", stdout_write);
+    module.register_fn("#%read", stdin_read);
+    module
+}
+
+pub async fn stdout_write() {
+    todo!()
+}
+
+pub async fn stdin_read() -> String {
+    todo!()
+}
